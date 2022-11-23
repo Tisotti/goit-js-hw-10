9a13b4d95b5ccc93e5866501558af9477b764010
Tisotti <yana.albertivna@gmail.com>
2022-11-23 01:27:07 +0100

    ready to check

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const DEBOUNCE_DELAY = 300;
 
 const searchCountry = document.querySelector ('#search-box');
 const countryList = document.querySelector ('.country-list');
-const countryInfo = document.querySelector (".country-info");
+const countryInfo = document.querySelector ('.country-info');
 
 
 searchCountry.addEventListener ('input', Debounce (onSearchCountry, DEBOUNCE_DELAY));
@@ -53,25 +53,27 @@ if (countries.length === 1){
 }
 
 };
+
 const listCountry = (list) => list.reduce ((acc,item) => acc + countryMarkup (item), '');
 
 const countryMarkup = (({name, flags}) => {
     return `<li class='country-item'>
     <img src='${flags.svg}' alt='flag${name.official}' width = '60' height = '40'> ${name.official}</li>`
-});
-
-
+})
 
 
 const countryInfoMarkup = countrys => {
-    // const {capital,population,languages} = country[0];
     return countrys
-        .map(({ capital, population, languages}) =>
-        `<p class="country-info__text"><span>Capital:</span>${capital}</p>
-        <p class="country-info__text"><span>Population:</span>${population}</p>
-        <p class="country-info__text"><span>Languages:${Object.values(languages).join(', ')} </span></p>`)
+        .map(({  name, capital, population, flags, languages}) =>
+        `<img class="country-item__flag" src="${flags.svg}" alt="Flag of ${name.official}" width=40 ${name.official}>
+        <ul class="country-info__list">
+        <p class="country-info__text"><span>Capital: </span>${capital}</p>
+        <p class="country-info__text"><span>Population: </span>${population}</p>
+        <p class="country-info__text"><span>Languages: ${Object.values(languages).join(', ')} </span></p>`)
 
 };
+
+
 const notFound = () => {
     Notiflix.Notify.failure('Oops, there is no country with that name')
     
